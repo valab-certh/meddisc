673f5bf15410c74eea324f2eb2bb9315b0472333
Apostolos Dimoulakis <apo.dimu@gmail.com>
2023-12-11 13:23:23 +0200

    v1.2.0

--- src/static/client.js
+++ src/static/client.js
@@ -3,134 +3,229 @@ var button = document.getElementById('SubmitAnonymizationProcess');
 var slider = document.getElementById("DICOMRange");
 var n_uploaded_files;
 var dicom_pair_fps;
+var OpenSequences = [];
 
 
-async function UpdateDICOMDisplayAndTable(dcm_idx)
+function HideSequence(SequenceID)
 {
-    // FIX IT
-    function table_v2(RawDCMMetadataObject, CleanedDCMMetadataObject)
+    const OpenSequencesStringified = OpenSequences.map(JSON.stringify);
+    const FirstOccurence = OpenSequencesStringified.indexOf(JSON.stringify(SequenceID))
+    let style;
+
+    if (FirstOccurence == -1)
     {
-        function RecursiveLevelBuild(MetadataTable, RawDCMMetadataObjectLvN, CleanedDCMMetadataObjectLvN, LeftMarginLvN)
+        // Expand sequence
+        OpenSequences.push(SequenceID);
+        style = 'block';
+    }
+    else
+    {
+        // Contract sequence
+        OpenSequences.splice(FirstOccurence, 1);
+        style = 'none';
+    }
+    document.getElementById(JSON.stringify(SequenceID)).style.display = style
+}
+
+function table(RawDCMMetadataObject, CleanedDCMMetadataObject)
+{
+    function RecursiveLevelBuild(MetadataTable, RawDCMMetadataObjectLvN, CleanedDCMMetadataObjectLvN, OffesetLeftMarginLvN, ParentNodeIdx, OpenSequences)
+    {
+        let CurrentNodeIdx = ParentNodeIdx.slice();
+        const indentation_block_unit = `<div class="cell-expand-row-margin"></div>`;
+        let indentation_block;
+
+        CurrentNodeIdx.push(-1)
+
+        for (let tagID in RawDCMMetadataObjectLvN)
         {
-            for (let tagID in RawDCMMetadataObjectLvN)
+            let tagID_part1 = tagID.substring(0, 4);
+            let tagID_part2 = tagID.substring(4);
+            let tagID_ = `(${tagID_part1},${tagID_part2})`;
+            const vr = RawDCMMetadataObjectLvN[tagID].vr;
+            const name = RawDCMMetadataObjectLvN[tagID].name;
+            const raw_value = RawDCMMetadataObjectLvN[tagID].value;
+            let cleaned_value;
+            let tag_dropped;
+            let right_row_contents;
+
+
+            CurrentNodeIdx[CurrentNodeIdx.length - 1] += 1;
+
+            if (CleanedDCMMetadataObjectLvN.hasOwnProperty(tagID))
             {
-                const vr = RawDCMMetadataObjectLvN[tagID].vr;
-                const name = RawDCMMetadataObjectLvN[tagID].name;
-                const raw_value = RawDCMMetadataObjectLvN[tagID].value;
-                // const cleaned_value = CleanedDCMMetadataObjectLvN[tagID].value;
+                cleaned_value = CleanedDCMMetadataObjectLvN[tagID].value;
+                tag_dropped = false;
+            }
+            else
+            {
+                cleaned_value = '<TAG REMOVED>';
+                tag_dropped = true;
+            }
 
-                
-                if (vr === 'SQ')
+            if (tag_dropped)
+            {
+                style = ' style="background-color: rgba(200, 0, 0, 10%);"';
+                right_row_contents = 
+                `
+                `;
+            }
+            else
+            {
+                if (cleaned_value !== raw_value)
                 {
-                    MetadataTable += 
-                    `
-                        <div class="outer-row">
-                            <div class="cell-expand-row">-</div>
-                            <div class="inner-row">
-                                <div class="left-row">
-                                    <div class="cell-dcmtag-id">${tagID}</div>
-                                    <div class="cell-dcmtag-vr">${vr}</div>
-                                    <div class="cell-dcmtag-value"></div>
-                                    <div class="cell-dcmtag-name">${name}</div>
-                                </div>
-                                <div class="cell-vertical-separator"></div>
-                                <div class="right-row">
-                                    <div class="cell-dcmtag-id">${tagID}</div>
-                                    <div class="cell-dcmtag-vr">${vr}</div>
-                                    <div class="cell-dcmtag-value"></div>
-                                    <div class="cell-dcmtag-name">${name}</div>
-                                </div>
-                            </div>
-                        </div>
-                    `
-
-                    LeftMarginLvN += 20
-
-                    for (let ds_idx in raw_value)
-                    {
-                        MetadataTable += 
-                        `
-                            <div class="outer-row">
-                                <div class="cell-expand-row-margin"></div>
-                                <div class="inner-row">
-                                    <div class="left-row">
-                                        <div class="cell-dataset">
-                                            Dataset ${ds_idx}
-                                        </div>
-                                    </div>
-                                    <div class="cell-vertical-separator"></div>
-                                    <div class="right-row">
-                                        <div class="cell-dataset">
-                                            Dataset ${ds_idx}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        `
-
-                        let DCMMetadataObjectLvNp1 = raw_value[ds_idx]
-                        MetadataTable = RecursiveLevelBuild(MetadataTable, DCMMetadataObjectLvNp1, LeftMarginLvN + 20)
-                    }
+                    style = ' style="background-color: rgba(200, 0, 0, 10%);"';
                 }
                 else
                 {
+                    style = ' style="background-color: rgba(50,50,55,255);"';
+                }
+                right_row_contents = 
+                `
+                    <div class="cell-dcmtag-id"${style}>${tagID_}</div>
+                    <div class="cell-dcmtag-vr"${style}>${vr}</div>
+                    <div class="cell-dcmtag-value"${style}>${cleaned_value}</div>
+                    <div class="cell-dcmtag-name"${style}>${name}</div>
+                `;
+            }
+
+            if (vr === 'SQ')
+            {
+                indentation_block = indentation_block_unit.repeat(OffesetLeftMarginLvN);
+                MetadataTable += 
+                `
+                    <div class="outer-row" onclick="HideSequence(${JSON.stringify(CurrentNodeIdx)})">
+                        <div class="inner-row">
+                            <div class="left-row">
+                                ${indentation_block}
+                                <div class="cell-expand-row"${style}></div>
+                                <div class="cell-dcmtag-id"${style}>${tagID_}</div>
+                                <div class="cell-dcmtag-vr"${style}>${vr}</div>
+                                <div class="cell-dcmtag-value"${style}></div>
+                                <div class="cell-dcmtag-name"${style}>${name}</div>
+                            </div>
+                            <div class="cell-vertical-separator"></div>
+                            <div class="right-row">
+                                ${indentation_block}
+                                <div class="cell-dcmtag-id"${style}>${tagID_}</div>
+                                <div class="cell-dcmtag-vr"${style}>${vr}</div>
+                                <div class="cell-dcmtag-value"${style}></div>
+                                <div class="cell-dcmtag-name"${style}>${name}</div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+
+                MetadataTable = 
+                `
+                    ${MetadataTable}
+                    <div id="${JSON.stringify(CurrentNodeIdx)}" style="display: none;">
+                `;
+
+                CurrentNodeIdx.push(-1)
+
+                for (let ds_idx in raw_value)
+                {
+                    CurrentNodeIdx[CurrentNodeIdx.length - 1] += 1;
+                    indentation_block = indentation_block_unit.repeat(OffesetLeftMarginLvN+1);
                     MetadataTable += 
                     `
                         <div class="outer-row">
-                            <div class="cell-expand-row"></div>
                             <div class="inner-row">
                                 <div class="left-row">
-                                    <div class="cell-dcmtag-id">${tagID}</div>
-                                    <div class="cell-dcmtag-vr">${vr}</div>
-                                    <div class="cell-dcmtag-value">${raw_value}</div>
-                                    <div class="cell-dcmtag-name">${name}</div>
+                                    ${indentation_block}
+                                    <div class="cell-dataset">
+                                        Dataset ${ds_idx}
+                                    </div>
                                 </div>
                                 <div class="cell-vertical-separator"></div>
                                 <div class="right-row">
-                                    <div class="cell-dcmtag-id">${tagID}</div>
-                                    <div class="cell-dcmtag-vr">${vr}</div>
-                                    <div class="cell-dcmtag-value">${raw_value}</div>
-                                    <div class="cell-dcmtag-name">${name}</div>
+                                    ${indentation_block}
+                                    <div class="cell-dataset">
+                                        Dataset ${ds_idx}
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    `
+                    `;
+
+                    const RawDCMMetadataObjectLvN = raw_value[ds_idx];
+                    const CleanedDCMMetadataObjectLvN = cleaned_value[ds_idx];
+
+                    MetadataTable = RecursiveLevelBuild(MetadataTable, RawDCMMetadataObjectLvN, CleanedDCMMetadataObjectLvN, OffesetLeftMarginLvN+2, CurrentNodeIdx);
                 }
+                MetadataTable = 
+                `
+                        ${MetadataTable}
+                    </div>
+                `;
+
+                CurrentNodeIdx = CurrentNodeIdx.slice(0, CurrentNodeIdx.length-1);
             }
-
-            return MetadataTable;
-        };
-
-        let MetadataTable = 
-        `
-            <div class="outer-row">
-                <div class="cell-expand-row"></div>
-                <div class="inner-row">
-                    <div class="left-row">
-                        <div class="cell-dcmtag-id">Tag ID</div>
-                        <div class="cell-dcmtag-vr">VR</div>
-                        <div class="cell-dcmtag-value">Tag Value</div>
-                        <div class="cell-dcmtag-name">Tag Name</div>
+            else
+            {
+                indentation_block = indentation_block_unit.repeat(OffesetLeftMarginLvN);
+                MetadataTable += 
+                `
+                    <div class="outer-row">
+                        <div class="inner-row">
+                            <div class="left-row">
+                                ${indentation_block}
+                                <div class="cell-expand-row"${style}></div>
+                                <div class="cell-dcmtag-id"${style}>${tagID_}</div>
+                                <div class="cell-dcmtag-vr"${style}>${vr}</div>
+                                <div class="cell-dcmtag-value"${style}>${raw_value}</div>
+                                <div class="cell-dcmtag-name"${style}>${name}</div>
+                            </div>
+                            <div class="cell-vertical-separator"></div>
+                            <div class="right-row">
+                                ${indentation_block}
+                                ${right_row_contents}
+                            </div>
+                        </div>
                     </div>
-                    <div class="cell-vertical-separator"></div>
-                    <div class="right-row">
-                        <div class="cell-dcmtag-id">Tag ID</div>
-                        <div class="cell-dcmtag-vr">VR</div>
-                        <div class="cell-dcmtag-value">Tag Value</div>
-                        <div class="cell-dcmtag-name">Tag Name</div>
-                    </div>
-                </div>
-            </div>
-        `;
-        let LeftMarginLv0 = 0;
-        let RawDCMMetadataObjectLv0 = RawDCMMetadataObject;
-        let CleanedDCMMetadataObjectLv0 = CleanedDCMMetadataObject;
-
-        MetadataTable = RecursiveLevelBuild(MetadataTable, RawDCMMetadataObjectLv0, CleanedDCMMetadataObjectLv0, LeftMarginLv0);
+                `;
+            }
+        }
 
         return MetadataTable;
     };
 
+    let UppermostRowStyle = ` style="background-color: rgba(45,45,50,255);"`;
+
+    let MetadataTable = 
+    `
+        <div class="outer-row">
+            <div class="inner-row">
+                <div class="left-row">
+                    <div class="cell-expand-row"></div>
+                    <div class="cell-dcmtag-id"${UppermostRowStyle}><b>Tag ID</b></div>
+                    <div class="cell-dcmtag-vr"${UppermostRowStyle}><b>VR</b></div>
+                    <div class="cell-dcmtag-value"${UppermostRowStyle}><b>Tag Value</b></div>
+                    <div class="cell-dcmtag-name"${UppermostRowStyle}><b>Tag Name</b></div>
+                </div>
+                <div class="cell-vertical-separator"></div>
+                <div class="right-row">
+                    <div class="cell-dcmtag-id"${UppermostRowStyle}><b>Tag ID</b></div>
+                    <div class="cell-dcmtag-vr"${UppermostRowStyle}><b>VR</b></div>
+                    <div class="cell-dcmtag-value"${UppermostRowStyle}><b>Tag Value</b></div>
+                    <div class="cell-dcmtag-name"${UppermostRowStyle}><b>Tag Name</b></div>
+                </div>
+            </div>
+        </div>
+    `;
+    const OffesetLeftMarginLv0 = 0;
+    const ParentNodeIdx = [];
+    const RawDCMMetadataObjectLv0 = RawDCMMetadataObject;
+    const CleanedDCMMetadataObjectLv0 = CleanedDCMMetadataObject;
+
+    MetadataTable = RecursiveLevelBuild(MetadataTable, RawDCMMetadataObjectLv0, CleanedDCMMetadataObjectLv0, OffesetLeftMarginLv0, ParentNodeIdx, OpenSequences);
+
+    return MetadataTable;
+};
+
+async function UpdateDICOMDisplayAndTable(dcm_idx)
+{
     const dicom_pair_fp = dicom_pair_fps[dcm_idx]
     const metadata_response = await fetch
     (
@@ -148,7 +243,7 @@ async function UpdateDICOMDisplayAndTable(dcm_idx)
     if (metadata_response.ok)
     {
         const dicom_pair = await metadata_response.json();
-        const dicom_metadata_table = table_v2(dicom_pair['raw_dicom_metadata'], dicom_pair['cleaned_dicom_metadata']);
+        const dicom_metadata_table = table(dicom_pair['raw_dicom_metadata'], dicom_pair['cleaned_dicom_metadata']);
         const raw_dicom_img_fp = dicom_pair['raw_dicom_img_fp']
         const cleaned_dicom_img_fp = dicom_pair['cleaned_dicom_img_fp']
 
@@ -163,19 +258,28 @@ async function UpdateDICOMDisplayAndTable(dcm_idx)
 
         document.getElementById('ConversionResult').innerHTML = 
         `
-            <center>
-                <div style="font-weight: bold;">Raw</div>
-            </center>
-            <center>
-                <div style="font-weight: bold;">Clean</div>
-            </center>
-            <div style="border: 1px solid black; padding: 10px;">
-                <img src="${raw_dicom_img_fp}" alt="Image 1" style="width: 100%; height: auto; object-fit: cover;">
+            <div style="display: flex; justify-content: space-between;">
+                <div class="cell-expand-row" style="border: none">
+                </div>
+                <div style="flex: 1;">
+                    <center>
+                        <div style="font-weight: bold;">Raw</div>
+                    </center>
+                    <div style="border: 1px solid black; padding: 10px; background-color: rgba(50,50,55,255); border-radius: 3px;">
+                        <img src="${raw_dicom_img_fp}" alt="Image 1" style="width: 100%; height: auto; object-fit: cover;">
+                    </div>
+                </div>
+                <div class="cell-vertical-separator"></div>
+                <div style="flex: 1;">
+                    <center>
+                        <div style="font-weight: bold;">Clean</div>
+                    </center>
+                    <div style="border: 1px solid black; padding: 10px; background-color: rgba(50,50,55,255); border-radius: 3px;">
+                        <img src="${cleaned_dicom_img_fp}" alt="Image 2" style="width: 100%; height: auto; object-fit: cover;">
+                    </div>
+                </div>
             </div>
-            <div style="border: 1px solid black; padding: 10px;">
-                <img src="${cleaned_dicom_img_fp}" alt="Image 2" style="width: 100%; height: auto; object-fit: cover;">
-            </div>
-            <div id="ConversionResult" class="metadata-table">
+            <div id="MetadataTable" class="metadata-table">
                 ${dicom_metadata_table}
             </div>
         `;
